c33963c875f5db99ee54cdb8778896a50d3bf7e6
Renner Costa <ozielrennerc.rodrigues@gmail.com>
2023-08-03 23:59:34 -0300

    adicionando arquivos de tipagem e pasta de animações

---
+++ src/Types/products.ts
@@ -0,0 +1,13 @@
+export interface CategoryProps{
+    id: number;
+    name: string;
+}
+
+export interface ProductsProps{
+    id: string | number;
+    title: string;
+    price: number;
+    category: CategoryProps,
+    available_quantity: number;
+    url_img: string;
+}
